063139ec4e4ec93ca68cfe87c36373991e7e4570
o-thiago <119901046+o-thiago@users.noreply.github.com>
2023-04-23 22:25:28 -0400

    FizzBuzz com overengineering

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::fmt::Display;
+
+use strum_macros::IntoStaticStr;
+
+#[derive(IntoStaticStr)]
+enum FizzBuzz {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Other(i32),
+}
+
+impl FizzBuzz {
+    const FIZZ: i32 = 3;
+    const BUZZ: i32 = 5;
+}
+
+impl From<&FizzBuzz> for String {
+    fn from(val: &FizzBuzz) -> Self {
+        match val {
+            FizzBuzz::Other(n) => n.to_string(),
+            v => Into::<&'static str>::into(v).to_string(),
+        }
+    }
+}
+
+impl Display for FizzBuzz {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&String::from(self))
+    }
+}
+
+impl From<i32> for FizzBuzz {
+    fn from(val: i32) -> Self {
+        match val {
+            v if v % (Self::FIZZ * Self::BUZZ) == 0 => Self::FizzBuzz,
+            v if v % Self::FIZZ == 0 => Self::Fizz,
+            v if v % Self::BUZZ == 0 => Self::Buzz,
+            v => Self::Other(v),
+        }
+    }
+}
+
+fn main() {
+    for value in (1..101).map(Into::<FizzBuzz>::into) {
+        println!("{}", Into::<String>::into(&value));
+    }
+}
